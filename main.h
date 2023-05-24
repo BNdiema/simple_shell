5d4ab41b6fb084882fbedd47d960e1a31b49de7e
BNdiema <bryantndiema@gmail.com>
2023-05-24 10:04:37 -0700

    fixed line 14 foo* bar to foo *bar

--- main.h
+++ main.h
@@ -11,7 +11,7 @@ void handle_command(char *cmd, char **env);
 void handle_arguments(char *cmd);
 char *get_path(const char *cmd);
 void print_environment(char **env);
-char *create_full_path(const char* dir, const char* cmd);
+char *create_full_path(const char *dir, const char *cmd);
 char *get_environment_variable(const char *name, char **env);
 int set_environment_variable(const char *name, const char *value);
 
